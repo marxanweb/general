91378372787b580d3e4105316f77e0492dcb3fa4
Andrew Cottam <a.cottam@gmail.com>
2019-05-09 10:31:33 +0200

    Create marxan.js

---
+++ registry/marxan.js
@@ -0,0 +1,11 @@
+var MAPBOX_BASEMAPS = [{name: 'Streets', description: 'A complete basemap, perfect for incorporating your own data.', id:'mapbox/streets-v10', provider:'mapbox'},
+    {name: 'Outdoors', description: 'General basemap tailored to hiking, biking, and sport.', id:'mapbox/outdoors-v10', provider:'mapbox'},
+    {name: 'Dark', description: 'Subtle dark backdrop for data visualizations.', id:'mapbox/dark-v9', provider:'mapbox'},
+    {name: 'Light', description: 'Subtle light backdrop for data visualizations.', id:'mapbox/light-v9', provider:'mapbox'},
+    {name: 'North Star', description: 'Slightly modified North Star with no Bathymetry.', id:'blishten/cjg6jk8vg3tir2spd2eatu5fd', provider:'Joint Research Centre'},
+    {name: 'Satellite', description: 'A beautiful global satellite and aerial imagery layer.', id:'mapbox/satellite-v9', provider:'mapbox'},
+    {name: 'Satellite Streets', description: 'Global imagery enhanced with road and label hierarchy.', id:'mapbox/satellite-streets-v9', provider:'mapbox'}];
+var MARXAN_SERVERS = [{name: 'Joint Research Centre, Italy', host: 'marxan-server-blishten.c9users.io', description: 'Central service for BIOPAMA',type:'remote'},
+                      {name: 'The Nature Conservancy, USA', host: 'pending', description: 'Central service for TNC',type:'remote'},
+                      {name: 'SPREP Regional Hub, Samoa', host: 'nonexistant', description: 'Regional node for BIOPAMA',type:'remote'},
+                      {name:'Beta test',host: 'andrewcottam.com',description:'v0.6 on Google Cloud Platform', type:'remote'}]
