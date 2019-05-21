fced6ed0db13a5a8275bb0e618f8b764c02136bc
Andrew Cottam <a.cottam@gmail.com>
2019-05-21 10:24:07 +0200

    Update marxan.js

--- registry/marxan.js
+++ registry/marxan.js
@@ -6,4 +6,6 @@ var MAPBOX_BASEMAPS = [{name: 'Streets', description: 'A complete basemap, perfe
     {name: 'Satellite', description: 'A beautiful global satellite and aerial imagery layer.', id:'mapbox/satellite-v9', provider:'mapbox'},
     {name: 'Satellite Streets', description: 'Global imagery enhanced with road and label hierarchy.', id:'mapbox/satellite-streets-v9', provider:'mapbox'}];
 var MARXAN_SERVERS = [{name: 'JRC Development Server', host: 'marxan-server-blishten.c9users.io', description: 'Main development server for Marxan Web.',type:'remote'},
+                      {name: 'The Nature Conservancy, USA', host: 'pending', description: 'Central service for TNC',type:'remote'},
+                      {name: 'SPREP Regional Hub, Samoa', host: 'nonexistant', description: 'Regional node for BIOPAMA',type:'remote'},
                       {name:'Beta test',host: 'andrewcottam.com',description:'v0.6 on Google Cloud Platform', type:'remote'}]
