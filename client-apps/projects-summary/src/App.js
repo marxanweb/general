20c9d3af576470c8c486561fe1b25d7c40a285e5
Andrew Cottam <a.cottam@gmail.com>
2019-05-22 16:26:57 +0000

    projects-summary build

--- client-apps/projects-summary/src/App.js
+++ client-apps/projects-summary/src/App.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import './App.css';
+import jsonp from 'jsonp-promise';
 
 const Map = ReactMapboxGl({
   accessToken: 'pk.eyJ1IjoiYmxpc2h0ZW4iLCJhIjoiMEZrNzFqRSJ9.0QBRA2HxTb8YHErUFRMPZg'
 });
 
 class App extends Component {
+  loadProjects(){
+    jsonp("https://marxan-server-blishten.c9users.io:8081/marxan-server/getProjectsWithGrids?&callback=__jp2").promise.then((response) => {
+      console.log(response);
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -14,15 +20,10 @@ class App extends Component {
       // eslint-disable-next-line
         style="mapbox://styles/mapbox/streets-v9"
         containerStyle={{
-          height: "100vh",
-          width: "100vw"
-        }}>
-          <Layer
-            type="symbol"
-            id="marker"
-            layout={{ "icon-image": "marker-15" }}>
-            <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
-          </Layer>
+          height: "800px",
+          width: "800px"
+        }}
+        onStyleLoad={this.loadProjects()}>
       </Map>      
     </div>
     );
