85f09eaae4a619172868a47c3996b77c76532969
Andrew Cottam <a.cottam@gmail.com>
2019-06-13 14:16:57 +0200

    Update marxan.js

--- registry/marxan.js
+++ registry/marxan.js
@@ -5,8 +5,8 @@ var MAPBOX_BASEMAPS = [{name: 'Streets', description: 'A complete basemap, perfe
     {name: 'North Star', description: 'Slightly modified North Star with no Bathymetry.', id:'blishten/cjg6jk8vg3tir2spd2eatu5fd', provider:'Joint Research Centre'},
     {name: 'Satellite', description: 'A beautiful global satellite and aerial imagery layer.', id:'mapbox/satellite-v9', provider:'mapbox'},
     {name: 'Satellite Streets', description: 'Global imagery enhanced with road and label hierarchy.', id:'mapbox/satellite-streets-v9', provider:'mapbox'}];
-var MARXAN_SERVERS = [{name: 'JRC Development Server', host: 'marxan-server-blishten.c9users.io', description: 'Main development server for Marxan Web.',type:'remote'},
-                      {name: 'The Nature Conservancy, USA', host: 'pending', description: 'Central service for TNC',type:'remote'},
-                      {name: 'SPREP Regional Hub, Samoa', host: 'nonexistant', description: 'Regional node for BIOPAMA',type:'remote'},
-                      {name:'Beta test (8081)',host: 'andrewcottam.com:8081',description:'Beta test Marxan Server hosted on Google Cloud Platform on port 8080. All projects will be deleted at the end.', type:'remote'},
-                     {name:'Beta test (8080)',host: 'andrewcottam.com',description:'Beta test Marxan Server hosted on Google Cloud Platform on port 8080. All projects will be deleted at the end.', type:'remote'}]
+var MARXAN_SERVERS = [{name: 'JRC Development Server', host: 'marxan-server-blishten.c9users.io', port:8080,description: 'Main development server for Marxan Web.',type:'remote'},
+                      {name: 'The Nature Conservancy, USA', host: 'pending', port:8080,description: 'Central service for TNC',type:'remote'},
+                      {name: 'SPREP Regional Hub, Samoa', host: 'nonexistant', port:8080,description: 'Regional node for BIOPAMA',type:'remote'},
+                      {name:'Beta test (8081)',host: 'andrewcottam.com',port:8081,description:'Beta test Marxan Server hosted on Google Cloud Platform on port 8080. All projects will be deleted at the end.', type:'remote'},
+                     {name:'Beta test (8080)',host: 'andrewcottam.com',port:8080,description:'Beta test Marxan Server hosted on Google Cloud Platform on port 8080. All projects will be deleted at the end.', type:'remote'}]
