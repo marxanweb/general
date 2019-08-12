b83fae513617315f206028bad41b2e1ada14ef3d
Andrew Cottam <a.cottam@gmail.com>
2019-08-12 14:25:21 +0200

    Update marxan.js

--- registry/marxan.js
+++ registry/marxan.js
@@ -9,4 +9,4 @@ var MARXAN_SERVERS = [{name: 'JRC Development Server', port: 8081, host: '61c92e
                       {name: 'The Nature Conservancy, USA', port: 8080, host: 'pending', protocol:'https:', description: 'Central service for TNC',type:'remote'},
                       {name: 'SPREP Regional Hub, Samoa', port: 8080, host: 'nonexistant', protocol:'https:', description: 'Regional node for BIOPAMA',type:'remote'},
                      {name:'Beta test', port: 8080, host: 'andrewcottam.com',protocol:'https:', description:'Beta test Marxan Server hosted on Google Cloud Platform. All projects will be deleted at the end.', type:'remote'}];
-var WDPA_VERSION = {version_date:'August 2019',downloadUrl:'https://www.protectedplanet.net/downloads/WDPA_Aug2019?type=shapefile'};
+var WDPA_VERSION = {version_date:'August 2019',downloadUrl:'https://github.com/andrewcottam/marxan-web/releases/download/beta/kinabalu.zip', tilesUrl:'https://storage.googleapis.com/geeimageserver.appspot.com/vectorTiles/wdpa/tilesets/'};
