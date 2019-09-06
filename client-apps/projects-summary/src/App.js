4cf5cf0acd057140ba866a04203f03c49d027d03
Andrew Cottam <a.cottam@gmail.com>
2019-09-06 15:28:10 +0000

    added popup to projects-summary

--- client-apps/projects-summary/src/App.js
+++ client-apps/projects-summary/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, {Popup , Layer, Feature } from "react-mapbox-gl";
 import './App.css';
 import jsonp from 'jsonp-promise';
 import parse from 'wellknown';
@@ -13,13 +13,14 @@ const Map = ReactMapboxGl({
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { features: [] };
+    this.state = { features: [],lat:0,lng:0,alias:'',display:'none' };
   }
   _handleMouseEnter(cursor, item) {
-    console.log(cursor && cursor.features && cursor.features[0] && cursor.features[0].properties && cursor.features[0].properties.alias);
+    var properties = cursor && cursor.features && cursor.features[0] && cursor.features[0].properties;
+    if (properties) this.setState({lng:cursor.lngLat.lng,lat:cursor.lngLat.lat, alias: properties.alias,display:'block'});
   }
-  _handleMouseLeave(cursor) {
-
+  _handleMouseLeave() {
+    this.setState({display:'none'});
   }
   loadProjects() {
     jsonp("https://andrewcottam.com:8080/marxan-server/getProjectsWithGrids?&callback=__jp2").promise.then((response) => {
@@ -50,11 +51,19 @@ class App extends Component {
             "fill-outline-color": "rgba(202,88,255,0.6)"
           }}
           onMouseEnter={(cursor, item) => this._handleMouseEnter(cursor, item)}
-          onMouseLeave={cursor => this._handleMouseLeave(cursor)}
+          onMouseLeave={() => this._handleMouseLeave()}
           children={c}
-          />
+        />
+        <Popup
+          style={{display:this.state.display}}
+          coordinates={[this.state.lng,this.state.lat]}
+          offset={{
+            'bottom-left': [12, -38],  'bottom': [0, -38], 'bottom-right': [-12, -38]
+          }}>
+          <h1>{this.state.alias}</h1>
+        </Popup>
       </Map>      
-    </div>
+      </div>
     );
   }
 }
