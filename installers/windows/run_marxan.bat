6d5812141b300dbd0fff1349ac1a48ce8364c6ff
andrewcottam <a.cottam@gmail.com>
2020-01-17 15:58:03 +0100

    fixes andrewcottam/marxan-web#136

--- installers/windows/run_marxan.bat
+++ installers/windows/run_marxan.bat
@@ -1,2 +1,2 @@
-@CALL conda.bat activate base
+@CALL conda.bat activate Miniconda3\envs\marxan
 @"%~dp0\Miniconda3\python.exe" "%~dp0\marxan-server\marxan-server.py" http://localhost:8080/index.html
