03c4238b440822ca8981c02622a80f62e2620975
andrewcottam <a.cottam@gmail.com>
2020-01-17 16:11:18 +0100

    bug fix in installer

--- installers/windows/python_prerequisites.bat
+++ installers/windows/python_prerequisites.bat
@@ -1,4 +1,4 @@
-CALL conda.bat --name marxan -y
+CALL conda.bat create --name marxan -y
 CALL conda.bat activate marxan
 "%~dp0\Miniconda3\Scripts\conda" install -y tornado psycopg2 pandas gdal colorama psutil
 "%~dp0Miniconda3\Scripts\pip" install mapbox -q
