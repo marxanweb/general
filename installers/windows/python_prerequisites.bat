5692da965ac0420a311bfe7355f59128e083dc83
andrewcottam <a.cottam@gmail.com>
2020-01-31 16:43:32 +0100

    Update python_prerequisites.bat

--- installers/windows/python_prerequisites.bat
+++ installers/windows/python_prerequisites.bat
@@ -1,3 +1,3 @@
 CALL conda.bat activate base
 "%~dp0Miniconda3\Scripts\conda" install -y tornado psycopg2 pandas gdal colorama psutil sqlalchemy 
-"%~dp0Miniconda3\Scripts\pip" install mapbox -q
+"%~dp0Miniconda3\Scripts\pip" install mapbox aiopg -q
