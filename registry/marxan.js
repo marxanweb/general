a760be399012de229d872c99d66c54ed4c20a15d
Andrew Cottam <a.cottam@gmail.com>
2020-02-10 11:36:06 +0100

    Update marxan.js

--- registry/marxan.js
+++ registry/marxan.js
@@ -1,4 +1,4 @@
-var CLIENT_VERSION = "v0.9.3";
+var CLIENT_VERSION = "v0.9.31";
 var SERVER_VERSION = "v0.9.3";
 var MAPBOX_BASEMAPS = [{name: 'Charted', alias:'ESRI Charted Territory',description: 'Oooo lovely', id:'https://www.arcgis.com/sharing/rest/content/items/1c365daf37a744fbad748b67aa69dac8/resources/styles/root.json', provider:'esri'},
     {name: 'Topographic', alias:'ESRI Topographic',description: 'Oooo lovely', id:'https://www.arcgis.com/sharing/rest/content/items/0f52cd2d17ea4773944a1d0e0fb99ea4/resources/styles/root.json', provider:'esri'},
