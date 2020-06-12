f304eda47bd2e405bb50e7a8310ef9cf5d569414
Andrew Cottam <a.cottam@gmail.com>
2020-06-12 07:10:07 +0000

    added registry json file

--- client-apps/url-testing/GET_test.py
+++ client-apps/url-testing/GET_test.py
@@ -5,7 +5,8 @@ from urllib.parse import urlparse
 
 #ENTER THE URL TO TEST HERE###########################################################################################
 # TEST_URL = "https://andrewcottam.com/marxan-server/getPUData?user=valeriepmculty&project=Test%20Project%20Val&puid=17917"
-TEST_URL = "http://localhost:8081/marxan-server/addParameter?type=server&key=ENABLE_RESET&value=true"
+# TEST_URL = "http://localhost:8081/marxan-server/addParameter?type=server&key=ENABLE_RESET&value=false"
+TEST_URL = "http://localhost:8081/marxan-server/getProject?user=admin&project=Coral%20Triangle%20Case%20Study"
 
 #authenticate
 parsed = urlparse(TEST_URL)
